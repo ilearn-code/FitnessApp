3aed217f42ca5cde7006fbfe4fb0dda3b4128495
myvcs <satyamyadavv931@gmail.com>
2022-04-26 00:12:06 +0530

    commit account

--- app/src/main/java/com/example/bmibuttomnav/FourthFragment.java
+++ app/src/main/java/com/example/bmibuttomnav/FourthFragment.java
@@ -1,15 +1,14 @@
 package com.example.bmibuttomnav;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import com.example.bmibuttomnav.R;
-
-import java.io.*;
 
 public class FourthFragment extends Fragment {
 
@@ -21,5 +20,15 @@ public class FourthFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_fourth, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Button button = (Button) getView().findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), account.class));
+            }
+        });
     }
 }
